b82d478b7035c5e2c393857a1a5f7caf80340421
White_cola <47351250+itscola@users.noreply.github.com>
2025-07-08 21:50:17 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,9 @@
 rootProject.name = "PlotSquared"
 
-include("Core", "Bukkit")
+// 保证 include 的名字和目录一致，且和 build.gradle.kts 里引用一致
+include("Core")
+include("Bukkit")
 
-project(":Core").name = "plotsquared-core"
-project(":Bukkit").name = "plotsquared-bukkit"
+// 不要重命名 project.name，保持目录名和 include 名一致
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
